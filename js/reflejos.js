b5c393caf7dc2c6a2902c22266b9e94bb306f147
InKu3uS <Neftarguez@gmail.com>
2023-02-16 19:13:58 +0000

    Solucionado problema del contador en el modo de juego normal

--- js/reflejos.js
+++ js/reflejos.js
@@ -1,11 +1,17 @@
-document.getElementById("comenzar").addEventListener("click", comenzarJuego);
+let tipoJuego = document.getElementById("comenzar");
+if(tipoJuego.className === "tiempo"){
+    document.getElementById("comenzar").addEventListener("click", tiempo);
+}else{
+    document.getElementById("comenzar").addEventListener("click", comenzarJuego);
+}
+
 let dificultad = document.getElementById("comenzar").className;
 
-if(dificultad == "facil") {
+if (dificultad == "facil") {
     dificultad = 2500;
-}else if(dificultad == "media"){
+} else if (dificultad == "media") {
     dificultad = 2000;
-}else{
+} else {
     dificultad = 1000;
 }
 
@@ -25,6 +31,15 @@ lista = [
  * Cojo la lista de paneles
  */
 let paneles = document.querySelectorAll(".paneles p");
+
+/**
+ * Se le añade a cada panel un evento "onclick" llamando a la funcion "controlarClick"
+ */
+paneles.forEach(panel=>{
+    panel.addEventListener("click", function(e){
+        controlarClick(e);
+    })
+});
 
 /**
  * Funcion que elimina el boton y el input al comenzar el juego
@@ -56,16 +71,48 @@ function mostrarPaneles() {
     })
 }
 
+/**
+ * Funcion que se encarga de iniciar el juego
+ */
 function comenzarJuego() {
     mostrarPaneles();
     ocultarTutorial();
     eliminarBoton();
     cambiarPaneles();
-
 }
-let puntos = 0;
-function cambiarPaneles() {
 
+//Contador de puntos
+let puntos = 0;
+/**
+ * Funcion que se encarga de incrementar el contador
+ */
+function incrementarContador() {
+    puntos++;
+}
+
+/**
+ * Funcion que comprueba la clase y el textContent del panel que dispara el evento.
+ * Si la clase y el textContent tienen el mismo valor llama a "incrementarContador"
+ * y actualiza el conteo de puntos.
+ * @param {*} e panel que dispara el evento.
+ */
+function controlarClick(e) {
+    let id = e.target.className;
+    let valor = e.target.textContent;
+    if (id === valor) {
+        incrementarContador();
+        document.getElementById("cuenta").innerHTML = `Vas ${puntos} aciertos`;
+    }
+}
+
+/**
+ * Funcion que recorre la lista de paneles, por cada panel en la lista
+ * coge un color aleatorio de la lista y lo añade como "textContent" del panel.
+ * Luego coge otro color aleatorio de la lista y lo añade como clase del panel.
+ * 
+ * Pasado el tiempo que corresponda a la dificultad, se vuelve a llamar a si misma.
+ */
+function cambiarPaneles() {
     paneles.forEach(panel => {
         let valorAleat = Math.floor(Math.random() * lista.length);
         let valor = lista[valorAleat];
@@ -74,16 +121,6 @@ function cambiarPaneles() {
         let claseAleat = Math.floor(Math.random() * lista.length);
         let clase = lista[claseAleat];
         panel.className = clase;
-
-        panel.addEventListener("click", function (e) {
-            let id = e.target.className;
-            let valor = e.target;
-            if (id === valor.textContent) {
-                puntos++;
-                console.log(puntos);
-                document.getElementsByClassName("contador")[0].innerHTML = "Vas " + puntos + " aciertos";
-            }
-        });
     });
     setTimeout(cambiarPaneles, dificultad);
 }
@@ -105,6 +142,17 @@ document.getElementById('id-moon').onclick = function () {
     document.getElementById('estilos').href = "styles/style-dark.css";
     this.classList.add('active')
 }
+
+function tiempo(){
+    let tiempo = document.getElementById("tiempo").value;
+    tiempo = tiempo*60000;
+    let limite = new Date();
+    limite.setTime(limite.getTime()+tiempo);
+    let fecha = new Date();
+    resta = limite.getTime()-fecha.getTime();
+    console.log(resta)
+}
+
 
 
 
